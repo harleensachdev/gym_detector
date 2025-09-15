4e614191225acc478cf480233a4c93cf757f9898
harleensachdev <blackberezz@gmail.com>
2025-09-15 20:56:41 +0800

    update dockerfile

--- app.py
+++ app.py
@@ -336,6 +336,7 @@ if __name__ == '__main__':
     logger.info("Server will start immediately. ML features will be installed on first use.")
     
     port = int(os.environ.get('PORT', 5000))
+    logger.info(f"Starting server on port {port}")
     
     app.run(
         host='0.0.0.0',
